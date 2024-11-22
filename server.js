8c1f41d8fbf852854bb91abd7f2bd1badbfa8426
CDKG810 <162853670+CDKG810@users.noreply.github.com>
2024-11-22 18:58:20 -0500

    update resp

---
+++ server.js
@@ -0,0 +1,47 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method);
+
+    //set header content type
+    res.setHeader('content-Type', 'text/html')
+
+    let path = './views/';
+    switch(req.url){
+        case '/':
+            path += 'index.html';
+            res.statusCode = 200;
+            break;
+        case '/about':
+            path += 'about.html';
+            res.statusCode = 200;
+            break;
+        case '/about-me':
+                path += 'about.html';
+                res.statusCode = 301;
+                res.setHeader('location', '/about');
+                res.end();
+                break;
+        default:
+            path += '404.html';
+            res.statusCode = 404;
+            break;
+    }
+
+    // send a html file
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end();
+        } else {
+           //res.write(data);
+            res.statusCode = 200;
+            res.end(data);
+        }
+    })
+});
+
+server.listen(3000, 'localhost', () => {
+    console.log('listening for requests on port 3000')
+})
